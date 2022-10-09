da3c3d86e6c81085b7a97447f0424b90c87f6f49
Kasia <kasia@cwiakalska.com>
2022-10-09 18:57:50 +0200

    Initial project setup with main page

---
+++ src/main/java/pl/jcw/menuwizard/MenuWizardApplication.java
@@ -0,0 +1,13 @@
+package pl.jcw.menuwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuWizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuWizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jcw/menuwizard/MenuWizardApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jcw.menuwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuWizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
